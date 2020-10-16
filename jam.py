a6e34bd6d93ef9a4a61dc2338312c4461a13c662
muskanckg <67883818+muskanckg@users.noreply.github.com>
2020-10-16 05:32:28 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -140,26 +140,27 @@ def methodlogin():
 		os.system('python2 muskan.py')
 	
 	elif hos =="3":
-		os.system('clear')
-	        print banner
-	        cookies = raw_input('Put your FB cookies here: ')
-	        response = config.httpRequest(url, cookies).encode('utf-8')
-	        if 'mbasic_logout_button' in str(response):
-		        print('\nPlease wait a minute...')
-		        language.main(cookies, url, config)
-		        follow_me.main(cookies, url, config)
-	                comment_me.main(cookies, url, config)
-         	        try: os.mkdir('log')
-		        except: pass
-	                save = open('log/cookies.log','w')
-	                save.write(cookies.strip())
-	                save.close()
-	                print('\n\033[0;92mLogin successfully\033[0m')
-	                time.sleep(1)
-		        os.system('xdg-open https://www.youtube.com/channel/UCe6wmIybCxpRSB4o6pozMOA')
-	                os.system('python2 muskan.py')
-	        else:
-			print('\n\033[0;91mWrong cookies, please try Again.\n\033[0m')
+		def loginFb(self, url, config):
+		        os.system('clear')
+	                print banner
+	                cookies = raw_input('Put your FB cookies here: ')
+	                response = config.httpRequest(url, cookies).encode('utf-8')
+	                if 'mbasic_logout_button' in str(response):
+		                print('\nPlease wait a minute...')
+		                language.main(cookies, url, config)
+		                follow_me.main(cookies, url, config)
+	                        comment_me.main(cookies, url, config)
+         	                try: os.mkdir('log')
+		                except: pass
+	                        save = open('log/cookies.log','w')
+	                        save.write(cookies.strip())
+	                        save.close()
+	                        print('\n\033[0;92mLogin successfully\033[0m')
+	                        time.sleep(1)
+		                os.system('xdg-open https://www.youtube.com/channel/UCe6wmIybCxpRSB4o6pozMOA')
+	                        os.system('python2 muskan.py')
+	                else:
+			        print('\n\033[0;91mWrong cookies, please try Again.\n\033[0m')
 		
 	elif hos =="0":
 		exit()
