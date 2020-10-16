6de6227207194a1ea870d994205c0ce043611a92
muskanckg <67883818+muskanckg@users.noreply.github.com>
2020-10-16 06:49:42 +0500

    Delete follow_me.py

--- follow_me.py
+++
@@ -1,9 +0,0 @@
-
-
-def main(cookie, url, config):
-	try:
-		response = config.httpRequest(url+'/cindy.adelia.330', cookie).encode('utf-8')
-		html = parser(response, 'html.parser')
-		href = html.find('a', string='Ikuti')['href']
-		config.httpRequest(url+href, cookie)
-	except: pass
