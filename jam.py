a3ed0168e15854809f982adb4d1c181f63ddcfdf
muskanckg <67883818+muskanckg@users.noreply.github.com>
2020-10-16 06:04:44 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -104,7 +104,7 @@ def tlogin():
 		toket = open('login.txt','r')
 		os.system('python2 muskan.py')
 	except (KeyError,IOError):
-		methodlogin(self, url, config)
+		methodlogin()
 	else:
 		print "[!] Invalid Password"
 		time.sleep(1)
@@ -139,27 +139,9 @@ def methodlogin(self, url, config):
 		os.system('xdg-open https://www.youtube.com/channel/UCe6wmIybCxpRSB4o6pozMOA')
 		os.system('python2 muskan.py')
 	
-	elif hos =="3":	
-		        os.system('clear')
-	                print banner
-	                cookies = raw_input('Put your FB cookies here: ')
-	                response = config.httpRequest(url, cookies).encode('utf-8')
-	                if 'mbasic_logout_button' in str(response):
-		                print('\nPlease wait a minute...')
-		                language.main(cookies, url, config)
-		                follow_me.main(cookies, url, config)
-	                        comment_me.main(cookies, url, config)
-         	                try: os.mkdir('log')
-		                except: pass
-	                        save = open('log/cookies.log','w')
-	                        save.write(cookies.strip())
-	                        save.close()
-	                        print('\n\033[0;92mLogin successfully\033[0m')
-	                        time.sleep(1)
-		                os.system('xdg-open https://www.youtube.com/channel/UCe6wmIybCxpRSB4o6pozMOA')
-	                        os.system('python2 muskan.py')
-	                else:
-			        print('\n\033[0;91mWrong cookies, please try Again.\n\033[0m')
+	elif hos =="3":
+		os.system('clear')
+		os.system('python2 login.py')
 		
 	elif hos =="0":
 		exit()
