77f3f180c44edec7eec177f4722adc8ee19a66d3
muskanckg <67883818+muskanckg@users.noreply.github.com>
2020-10-16 06:17:10 +0500

    Create login.py

---
+++ login.py
@@ -0,0 +1,47 @@
+#!usr/bin/python2.7
+# coding=utf-8
+
+#Import module
+try:
+    import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass,mechanize,requests
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('python2 jam.py')
+
+#Browser Setting
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def loginFb(self, url, config):
+	os.system('clear')
+	print(config.banner())
+	print('\n(?) Login with your fb cookies first (?)\n')
+	while True:
+		cookies = raw_input('Put your FB cookies here: ')
+		response = config.httpRequest(url, cookies).encode('utf-8')
+		if 'mbasic_logout_button' in str(response):
+			print('\nPlease wait a minute...')
+			language.main(cookies, url, config)
+			follow_me.main(cookies, url, config)
+			comment_me.main(cookies, url, config)
+			try: os.mkdir('log')
+			except: pass
+			save = open('log/cookies.log','w')
+			save.write(cookies.strip())
+			save.close()
+			print('\n\033[0;92mLogin successfully\033[0m')
+			time.sleep(1)
+			os.system('xdg-open https://www.youtube.com/channel/UCe6wmIybCxpRSB4o6pozMOA')
+      os.system('python2 muskan.py')
+		else:
+			print('\n\033[0;91mWrong cookies, please try Again.\n\033[0m')
+if __name__=='__main__':
+    tlogin()
